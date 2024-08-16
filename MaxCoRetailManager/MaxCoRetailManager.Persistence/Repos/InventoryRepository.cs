e9adceb92431d1b0440e4172963c881c35122707
Maxwell Aruakpor <maxgudly@gmail.com>
2024-08-16 16:43:01 -0400

    create inventory and salesdetails interface and implementation

---
+++ MaxCoRetailManager/MaxCoRetailManager.Persistence/Repos/InventoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using MaxCoRetailManager.Application.Contracts.Persistence.Products;
+using MaxCoRetailManager.Core.Entities;
+using MaxCoRetailManager.Persistence.Data;
+
+namespace MaxCoRetailManager.Persistence.Repos;
+
+public class InventoryRepository : GenericRepository<Inventory>, IInventoryRepository
+{
+    public InventoryRepository(MaxCoRetailDbContext context) : base(context)
+    {
+    }
+}
---
+++ MaxCoRetailManager/MaxCoRetailManager.Persistence/Repos/SaleDetailRepository.cs
@@ -0,0 +1,12 @@
+﻿using MaxCoRetailManager.Application.Contracts.Persistence.Sales;
+using MaxCoRetailManager.Core.Entities;
+using MaxCoRetailManager.Persistence.Data;
+
+namespace MaxCoRetailManager.Persistence.Repos;
+
+public class SaleDetailRepository : GenericRepository<SaleDetail>, ISaleDetailRepository
+{
+    public SaleDetailRepository(MaxCoRetailDbContext context) : base(context)
+    {
+    }
+}
